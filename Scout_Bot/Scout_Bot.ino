f5a92b6a03733a03ce6a9ffbbcd6839f128d68b2
AnkurKJain <99ankurjain@gmail.com>
2014-06-21 23:22:23 -0700

    Add Arduino Code

---
+++ Scout_Bot/Scout_Bot.ino
@@ -0,0 +1,57 @@
+const int trigpin = 11;
+const int echopin = 10;
+#define servo1 5
+#define servo2 6
+long Duration = 0;
+
+void setup()
+{
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  pinMode(servo1, OUTPUT);
+  pinMode(servo2, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+
+void loop()
+{
+  digitalWrite(trigpin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigpin, HIGH); 
+delayMicroseconds(10); 
+digitalWrite(trigpin, LOW);
+
+Duration = pulseIn(echopin, HIGH);
+long Distance_mm = Distance(Duration);
+Serial.print("Distance = "); 
+Serial.print(Distance_mm);
+Serial.println(" mm");
+delay(50);
+}
+
+long Distance(long time)
+{
+  long DistanceCalc;
+  DistanceCalc = ((time/2.9)/2);
+  
+  if (DistanceCalc < 150) {
+   analogWrite(servo1, -150);
+  analogWrite(servo2, 150);
+ delay (50);
+ 
+  } 
+    else {
+    analogWrite (servo2, -230);
+    analogWrite (servo1, 230);
+    
+    }  
+  
+  return DistanceCalc;
+}
+
+
+
+
+
